b8ab4bbc75fcb9e24d9896eaaf4cf21cb5a31c1e
Tibor Bodecs <mail.tib@gmail.com>
2024-04-16 10:25:21 +0200

    add pk constraint

---
+++ Sources/FeatherDatabase/Database/Table/Constraints/PrimaryKeyConstraint.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tibor Bodecs on 16/04/2024.
+//
+
+import SQLKit
+
+public struct PrimaryKeyConstraint: DatabaseConstraintInterface {
+    public let sqlConstraint: SQLTableConstraintAlgorithm
+
+    public init(_ column: any DatabaseColumnName) {
+        self.init([column])
+    }
+
+    public init(_ columns: [any DatabaseColumnName]) {
+        self.sqlConstraint = .primaryKey(
+            columns: columns.map { SQLIdentifier($0.rawValue) }
+        )
+    }
+}
